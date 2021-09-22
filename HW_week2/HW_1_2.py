428df44f7108d98a12879532c1989fc86e1a28db
ykimoh <yk.kimoh@gmail.com>
2021-09-22 14:58:23 -0400

    Create HW_1_2.py

---
+++ HW_week2/HW_1_2.py
@@ -0,0 +1,42 @@
+# This is for assignment1
+
+book = {
+    "title": "Mismatch",
+    "sub title": "How inclusion shapes design",
+    "author": "Kat Holmes",
+    "price": "$17.95",
+    "publishing company" : "The MIT Press"
+}
+
+
+phone = {
+    "name": "iPhone",
+    "version": "X",
+    "color": "Pink Gold",
+    "resolution": "812x375",
+    "screen size": "5.8 inch",
+}
+
+
+cup = {
+    "shop": "whitney",
+    "color": "green&pink",
+    "shape": "cylinder",
+    "madeIn" : "Porland",
+    "dishwasher" : False
+}
+
+sunglass = {
+    "brand" : "Ray Ban",
+    "type": "aviator",
+    "lenseColor": "darkgreen",
+    "frameColor": "gold",
+
+}
+
+print (book ["title"])
+print (phone["color"])
+print (cup["dishwasher"])
+print (sunglass["type"])
+print (book ["title"] + " " + phone["color"])
+
